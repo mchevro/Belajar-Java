ddc7f43d32fe5ab1367de3d4a64b4a52b18529c5
mchevro <mchevro19@gmail.com>
2022-03-26 22:33:18 +0700

    Rapihkan Folder

---
+++ PZN/TipeDataChar.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tutor_PZN;
+
+/**
+ *
+ * @author Mahendra Chevro
+ */
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char m = 'm';
+        char c = 'c';
+        
+        System.out.println(m);
+        System.out.println(c);
+    }
+}
